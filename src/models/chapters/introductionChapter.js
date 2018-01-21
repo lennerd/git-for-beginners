153a810a0203dcd883bd3ea3974455a0b59fbdb0
Lennart Hildebrandt <hello@lennerd.com>
2018-01-21 13:51:14 +0100

    Fix text formatting in introduction.

--- src/models/chapters/introductionChapter.js
+++ src/models/chapters/introductionChapter.js
@@ -10,14 +10,16 @@ const introductionChapter = createChapter('Introduction', {
     )),
     new ChapterText(() => (
       <Fragment>
-        To prevent data loss we create backups and use clouds to store files and share data with others. Two people working on the same file is often impossible though. And after all, data is lost, because we accidentily deleted an old file or have overwritten a file a college had changed a few minutes before. <strong>No matter how hard we work on file name conventions and how many channels we use to communicate in our team, mistakes are made.</strong>
+        To prevent data loss we create backups and use clouds to store files and share data with others. Two people working on the same file is often impossible though. And after all, data is lost, because we accidentily deleted an old file or have overwritten a file a college had changed a few minutes before. <em>No matter how hard we work on file name conventions and how many channels we use to communicate in our team, mistakes are made.</em>
       </Fragment>
     )),
     new ChapterText(() => (
       'But not everything is lost (pun intended). Special version control systems can help to store versions of our project more effectily and give our team a better way of working on files together.'
     )),
     new ChapterText(() => (
-      <strong>Welcome to “Git for Beginners” – an interactive tutorial to learn and understand Git, a popular version control system to help you and your team to not loose data again.</strong>
+      <Fragment>
+        Welcome to <strong>Git for Beginners</strong> – an interactive tutorial to learn and understand Git, a popular version control system to help you and your team to not loose data again.
+      </Fragment>
     )),
     new ChapterText(() => 'But let’s start by taking a look at …'),
   ],
