0bf7111710c8214e8f28f7d3c54775595a0023dc
Lennart Hildebrandt <hello@lennerd.com>
2018-01-25 16:52:50 +0100

    Simplify introduction text.

--- src/models/chapters/introductionChapter.js
+++ src/models/chapters/introductionChapter.js
@@ -1,27 +1,58 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 
-import { createChapter } from "../Chapter";
-import { ChapterText } from "../ChapterSection";
+import { createChapter } from '../Chapter';
+import { ChapterText } from '../ChapterSection';
 
 const introductionChapter = createChapter('Introduction', {
   sections: [
     new ChapterText(() => (
-      'Over the passed decades computer in different shape and sizes changed our daily life enormously. Together we create huge amount of data in form of files everyday to store everything from invoices to love letters, from code to illustrations and designs.'
-    )),
-    new ChapterText(() => (
       <Fragment>
-        To prevent data loss we create backups and use clouds to store files and share data with others. Two people working on the same file is often impossible though. And after all, data is lost, because we accidentily deleted an old file or have overwritten a file a college had changed a few minutes before. <em>No matter how hard we work on file name conventions and how many channels we use to communicate in our team, mistakes are made.</em>
+        Over the passed decades computer in different shape and sizes changed
+        our daily life enormously. Together we create huge amount of data in
+        form of files everyday to store everything from invoices to love
+        letters, from code to illustrations and designs.
       </Fragment>
     )),
     new ChapterText(() => (
-      'But not everything is lost (pun intended). Special version control systems can help to store versions of our project more effectily and give our team a better way of working on files together.'
+      <Fragment>
+        Although we create backups and store our files in clouds, data is lost
+        frequently, because we accidentily deleted an old file or have
+        overwritten a file a college had changed a few minutes before.<br />
+        <em>
+          No matter how hard we work on file name conventions and how many
+          channels and notes we create to communicate to our self and our team,
+          mistakes are made.
+        </em>
+      </Fragment>
+    )),
+    /*new ChapterText(() => (
+      <Fragment>
+        To prevent data loss we create backups and use clouds to store files and
+        share data with others. Two people working on the same file is often
+        impossible though. And after all, data is lost, because we accidentily
+        deleted an old file or have overwritten a file a college had changed a
+        few minutes before.{' '}
+        <em>
+          No matter how hard we work on file name conventions and how many
+          channels we use to communicate in our team, mistakes are made.
+        </em>
+      </Fragment>
+    )),*/
+    new ChapterText(() => (
+      <Fragment>
+        But not everything is lost (pun intended). Special version control
+        systems can help to store versions of our project more effectily and
+        give our team a better way of working on files together.
+      </Fragment>
     )),
     new ChapterText(() => (
       <Fragment>
-        Welcome to <strong>Git for Beginners</strong> – an interactive tutorial to learn and understand Git, a popular version control system to help you and your team to not loose data again.
+        Welcome to <strong>Git for Beginners</strong> – an interactive tutorial
+        to learn and understand Git, a popular version control system to help
+        you and your team to not loose data again.
       </Fragment>
     )),
-    new ChapterText(() => 'But let’s start by taking a look at …'),
+    new ChapterText(() => 'Let’s start by taking a look at …'),
   ],
 });
 
